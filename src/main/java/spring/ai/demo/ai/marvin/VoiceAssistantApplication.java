32d0e705bea7cfcafd8697c3b31924da8d7f6fd5
Christian Tzolov <christian.tzolov@broadcom.com>
2025-05-06 07:12:12 +0200

    align with latests sprign ai updates
    Signed-off-by: Christian Tzolov <christian.tzolov@broadcom.com>

--- src/main/java/spring/ai/demo/ai/marvin/VoiceAssistantApplication.java
+++ src/main/java/spring/ai/demo/ai/marvin/VoiceAssistantApplication.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.model.Media;
+import org.springframework.ai.content.Media;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -18,8 +18,10 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 
 /**
- * ChatBot Assistant Application that uses voice input and output to communicate with the
- * user. The application uses the simple {@link Audio} utility to record and playback the
+ * ChatBot Assistant Application that uses voice input and output to communicate
+ * with the
+ * user. The application uses the simple {@link Audio} utility to record and
+ * playback the
  * audio.
  *
  * @author Christian Tzolov
@@ -38,21 +40,22 @@ public class VoiceAssistantApplication {
 		return args -> {
 
 			// Create the ChatClient with with system prompt and conversation memory.
-			// The model and the audio formats are configured in the application.properties file.
+			// The model and the audio formats are configured in the application.properties
+			// file.
 			var chatClient = chatClientBuilder
-				.defaultSystem(systemPrompt)
-				.defaultAdvisors(new MessageChatMemoryAdvisor(new InMemoryChatMemory()))
-				.build();
+					.defaultSystem(systemPrompt)
+					.defaultAdvisors(new MessageChatMemoryAdvisor(MessageWindowChatMemory.builder().build()))
+					.build();
 
 			try (Scanner scanner = new Scanner(System.in)) {
 
-				// Audio utility to record and playback the audio.
+				// Audio utility to record and play back the audio.
 				Audio audio = new Audio();
 
 				// Start the chat loop
-				while (true) {					
-					//Record user's voice input
-					audio.startRecording(); 
+				while (true) {
+					// Record user's voice input
+					audio.startRecording();
 					System.out.print("\nRecording your question ... press <Enter> to stop! ");
 					scanner.nextLine();
 					audio.stopRecording();
@@ -61,13 +64,15 @@ public class VoiceAssistantApplication {
 
 					// Send user's input to the AI model and get the response
 					AssistantMessage response = chatClient.prompt()
-						.messages(new UserMessage("Please answer the questions in the audio input",
-								new Media(MediaType.parseMediaType("audio/wav"),
-										new ByteArrayResource(audio.getLastRecording()))))
-						.call()
-						.chatResponse()
-						.getResult()
-						.getOutput();
+							.messages(UserMessage.builder()
+									.text("Please answer the questions in the audio input")
+									.media(new Media(MediaType.parseMediaType("audio/wav"),
+											new ByteArrayResource(audio.getLastRecording())))
+									.build())
+							.call()
+							.chatResponse()
+							.getResult()
+							.getOutput();
 
 					// Print the text (e.g. transcription) response
 					System.out.println("\nASSISTANT: " + response.getText());
